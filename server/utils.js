d67eb20070d294d87057c1c44f84b4586991f15e
Willie Jeng <willie.jeng@gmail.com>
2019-04-28 20:28:38 -0700

    updated server file structure

--- server/utils.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.isValidEmail = function (iStr) {
-    var emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return emailRegex.test(iStr);
-};
